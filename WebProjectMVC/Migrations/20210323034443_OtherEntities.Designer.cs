f994ebc419894241aaf55edf7b421137010de412
DiegoManucci <diegobizzotto2003@gmail.com>
2021-03-23 01:00:21 -0300

    Second Migration

--- WebProjectMVC/Migrations/20210323034443_OtherEntities.Designer.cs
+++ WebProjectMVC/Migrations/20210323034443_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebProjectMVC.Data;
 
 namespace WebProjectMVC.Migrations
 {
     [DbContext(typeof(WebProjectMVCContext))]
-    partial class WebProjectMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20210323034443_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
