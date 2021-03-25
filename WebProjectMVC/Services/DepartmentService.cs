2a86e659eb60033a941cd3771e4f36fc57baf201
DiegoManucci <81128924+DiegoManucci@users.noreply.github.com>
2021-03-25 12:40:54 -0300

    Error Delete Seller

--- WebProjectMVC/Services/DepartmentService.cs
+++ WebProjectMVC/Services/DepartmentService.cs
@@ -11,6 +11,11 @@ namespace WebProjectMVC.Services
     {
         private readonly WebProjectMVCContext _context;
 
+        public DepartmentService(WebProjectMVCContext context)
+        {
+            _context = context;
+        }
+
         public List<Department> FindAll()
         {
             return _context.Department.OrderBy(x => x.Name).ToList();
--- WebProjectMVC/Services/SellerService.cs
+++ WebProjectMVC/Services/SellerService.cs
@@ -22,6 +22,18 @@ namespace WebProjectMVC.Services
             _context.SaveChanges();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
