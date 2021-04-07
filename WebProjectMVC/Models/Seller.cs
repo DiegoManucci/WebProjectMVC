d9aa10202df74735ac5f428ec62af118e2f336a8
DiegoManucci <81128924+DiegoManucci@users.noreply.github.com>
2021-04-07 13:29:56 -0300

    Validation

--- WebProjectMVC/Models/Seller.cs
+++ WebProjectMVC/Models/Seller.cs
@@ -9,18 +9,32 @@ namespace WebProjectMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "{0} size must be between {2} and {1}!")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required!")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Must be a valid Email!")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required!")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(0.0, 5000.0, ErrorMessage = "{0} must be between {1} and {2}!")]
         public double BaseSalary { get; set; }
+
+        [Required(ErrorMessage = "{0} required!")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
         public Department Department { get; set; }
+
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
